3ad6b89a9fb9021ce38b691728b9f93fe3fa5a2a
Batgard <remi.gatard@gmail.com>
2020-05-05 10:46:33 +0200

    Add POST method in HelloApiView
    Use Serializer to parse POST request content

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,9 +1,14 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
+from profiles_api import serializers
 
 class HelloApiView(APIView):
     """Test API View"""
+
+    serializer_class = serializers.HelloSerializer
+
     def get(self, request, format=None):
         """Return a list of APIView features"""
         an_apiView = [
@@ -18,3 +23,16 @@ class HelloApiView(APIView):
                 'an_api_view': an_apiView
             }
         )
+
+    def post(self, request):
+        """Create entry with the provided name"""
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(
+            serializer.errors,
+            status = status.HTTP_400_BAD_REQUEST
+            )
