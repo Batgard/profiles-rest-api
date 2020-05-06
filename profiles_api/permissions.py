2252f5460bf9259b508626a48a2521308cc3c064
Batgard <remi.gatard@gmail.com>
2020-05-06 15:16:42 +0200

    Create status model and its CRUD
    Also adding the right permissions / restrictions

--- profiles_api/permissions.py
+++ profiles_api/permissions.py
@@ -9,3 +9,14 @@ class UpdateOwnProfile(permissions.BasePermission):
             return True
 
         return obj.id == request.user.id
+
+class UpdateOwnStatus(permissions.BasePermission):
+    """Limit ProfileFeedItem's updates to user's owned"""
+
+    def has_object_permission(self, request, view, obj):
+        """Check update is made by logged user or is safe"""
+
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.user_profile.id == request.user.id
